c77a0ab3f74ff44bede9b3f1df929f6f33d9e247
prakhar1989 <prakhar1989@gmail.com>
2012-05-07 22:47:28 +0530

    Added templates and refactored

--- helloworld.py
+++ helloworld.py
@@ -1,184 +1,60 @@
+import sys
+import os
 import webapp2
-import cgi
+import jinja2
+import hashlib
+import hmac
+import random
 import string
-import re
 
-form = """
-<form method = "post">
-    What is your birthday?
-    <label>Month <input type="text" name="month" value=%(month)s> </label>
-    <label>Day <input type="text" name="day" value=%(day)s> </label>
-    <label>Year <input type="text" name="year" value=%(year)s> </label>
-    <div style="color:red">%(error)s</div>
-    <br>
-    <br>
-    <input type="submit">
-</form>
-"""
-rot_form = """
-<form method="post">
-    <textarea name="text" style="height: 100px; width: 200px;">%(placeholder)s</textarea>
-    <input type="submit">
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
-header = """
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Sign Up</title>
-    <link type="text/css" rel="stylesheet" href="/stylesheets/bootstrap.css" />
-    <style>
-        body { margin-top: 60px }
-        .error { color: red }
-    </style>
-</head>
-<body>
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="i-bar"></span>
-            <span class="i-bar"></span>
-            <span class="i-bar"></span>
-          </a>
-          <a class="brand" href="/">CS253</a>
-          <div class="nav-collapse">
-            <ul class="nav">
-              <li><a href="/unit2/signup">Signup</a></li>
-              <li><a href="/unit2/rot13">Rot13</a></li>
-              <li><a href="/about">About</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+import valid_helpers
 
-    <div class="container">
-"""
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-footer = """
-    </div>
-</body>
-</html>
-"""
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-signup_form = """
-<form method="post">
-    <div class="control-group">
-        <label class="control-label">Username</label>
-            <div class="controls">
-                <input type="text" name="username" value=%(user_username)s>
-                <span class="help-inline error">%(error_username)s</span>
-            </div>
-    </div>
-    
-    <div class="control-group">
-        <label class="control-label">Password</label>
-            <div class="controls">
-                <input type="password" name="password" value=%(user_password)s>
-                <span class="help-inline error">%(error_password)s</span>
-            </div>
-    </div>
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
-    <div class="control-group">
-        <label class="control-label">Verify Password</label>
-            <div class="controls">
-                <input type="password" name="verify" value=%(user_verify)s>
-                <span class="help-inline error">%(error_verify)s</span>
-            </div>
-    </div>
 
-    <div class="control-group ">
-        <label class="control-label">Email (Optional)</label>
-            <div class="controls">
-                <input type="text" name="email" value=%(user_email)s>
-                <span class="help-inline error">%(error_email)s</span>
-            </div>
-    </div>
-
-    <div class="actions">
-        <button type="submit" class="btn primary">Signup!</button>
-    </div>
-</form>
-"""
-
-def escape_html(s):
-    return cgi.escape(s, quote=True)
-
-def valid_month(month):
-      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-                'August', 'September', 'October', 'November', 'December']
-      if month.capitalize() in months:
-          return month.capitalize()
-      else:
-          return None
-
-def valid_day(day):
-    if day and day.isdigit():
-        if int(day) < 1 or int(day) > 31:
-            return None
-        else:
-            return int(day)
-
-def valid_year(year):
-    if year and year.isdigit():
-        if int(year) < 1900 or int(year) > 2020:
-            return None
-        else:
-            return int(year)
-
-class MainPage(webapp2.RequestHandler):
-
-  def write_form(self, error="", month="", day="", year=""):
-    self.response.out.write(form % {"error" : error, 
-                                    "month" : month, 
-                                    "day" : day, 
-                                    "year" : year })
+class MainPage(Handler):
 
   def get(self):
-    self.response.out.write(header)
-    self.write_form()
-    self.response.out.write(footer)
+    self.render("form.html")
 
   def post(self):
     user_month = self.request.get('month')
     user_day = self.request.get('day')
     user_year = self.request.get('year')
 
-    month = valid_month(user_month)
-    day = valid_day(user_day)
-    year = valid_year(user_year)
+    month = valid_helpers.valid_month(user_month)
+    day = valid_helpers.valid_day(user_day)
+    year = valid_helpers.valid_year(user_year)
 
     if not(day and month and year):
-      self.response.out.write(header)
-      self.write_form(error="Invalid data! Please re-enter",
-                      day = escape_html(user_day),
-                      month = escape_html(user_month),
-                      year = escape_html(user_year))
-      self.response.out.write(footer)
+      self.render("form.html", error="Invalid data! Please re-enter",
+                  day = user_day,
+                  month = user_month,
+                  year = user_year)
     else:
-      self.redirect("/thanks")
+      self.write("Thats valid")
 
-class ThanksHandler(webapp2.RequestHandler):
+class RotHandler(Handler):
     def get(self):
-        self.response.out.write(header)
-        self.response.out.write("Thanks! Thats valid")
-        self.response.out.write(footer)
-
-class RotHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write(header)
-        self.response.out.write("<h1>Rot13</h1>")
-        self.response.out.write(rot_form % {"placeholder" : ""})
-        self.response.out.write(footer)
+        self.render("rot_form.html")
 
     def post(self):
-        self.response.out.write(header)
-        user_string = self.request.get("text")
-        converted_string = self.rot13(user_string) 
-        self.response.out.write(rot_form % {"placeholder" : escape_html(converted_string)})
-        self.response.out.write(footer)
+        self.render("rot_form.html", 
+                placeholder = self.rot13(self.request.get("text")) )
 
     def rot13(self, s):
         alphabets = string.ascii_lowercase
@@ -192,126 +68,57 @@ class RotHandler(webapp2.RequestHandler):
             else:
                 s_index = alphabets.find(s[i])
             if s_index >= 0:
-                if s_index + 13 >= 26:
-                    if caps:
-                        output.append((alphabets[(s_index+13)%26]).upper())
-                    else:
-                        output.append(alphabets[(s_index+13)%26])
+                if caps:
+                    output.append((alphabets[(s_index+13)%26]).upper())
                 else:
-                    if caps:
-                        output.append((alphabets[s_index+13]).upper())
-                    else:
-                        output.append(alphabets[s_index+13])
+                    output.append(alphabets[(s_index+13)%26])
             else:
                 output.append(s[i])
             caps = False
             i = i + 1
         return "".join(output)
 
-class SignupHandler(webapp2.RequestHandler):
-
-    def valid_username(self, username):
-        USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-        if USER_RE.match(username):
-            return True
-        else:
-            return False
-
-    def valid_password(self, password):
-        PASS_RE = re.compile(r"^.{3,20}$")
-        if PASS_RE.match(password):
-            return True
-        else:
-            return False
-
-    def valid_verify(self, password, verify):
-        if password == verify:
-            return True
-        else:
-            return False
-
-    def valid_email(self, email):
-        if email == "":
-            return True
-        EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
-        if EMAIL_RE.match(email):
-            return True
-        else:
-            return False
-
-    def write_form(self, username="", password="", verify="", email="", error_username="", error_password="", error_email="", error_verify=""):
-        self.response.out.write(signup_form % {"user_username" : username, #TODO: ESCAPE HTML
-                                               "user_password" : password,
-                                               "user_verify" : verify,  
-                                               "user_email" : email,
-                                               "error_username" : error_username,
-                                               "error_password" : error_password,
-                                               "error_verify" : error_verify, 
-                                               "error_email" : error_email})
+class SignupHandler(Handler):
     def get(self):
-        self.response.out.write(header)
-        self.write_form()
-        self.response.out.write(footer)
+        self.render("signup_form.html")
 
     def post(self):
+        have_error = False
         user_username = self.request.get('username')
         user_password = self.request.get('password')
         user_verify = self.request.get('verify')
         user_email = self.request.get('email')
         
-        check_username = self.valid_username(user_username)
-        check_password = self.valid_password(user_password)
-        check_verify = self.valid_verify(user_verify, user_password)
-        check_email = self.valid_email(user_email)
+        check_username = valid_helpers.valid_username(user_username)
+        check_password = valid_helpers.valid_password(user_password)
+        check_verify = valid_helpers.valid_verify(user_verify, user_password)
+        check_email = valid_helpers.valid_email(user_email)
 
-        error_username = ""
-        error_password = ""
-        error_verify = ""
-        error_email = ""
+        params = dict(user_username = user_username, user_email = user_email)
 
-        if not(check_username and check_password and check_email and check_verify):
-            # self.write_form(error="Invalid data! Please re-enter",
-            #               day = cgi.escape(user_day, quote=True), 
-            #               month = cgi.escape(user_month, quote=True),
-            #               year = cgi.escape(user_year, quote=True))
-            self.response.out.write(header)
-            if not(check_username):
-                error_username = "That's not a valid username."
-            if not(check_password):
-                error_password = "That wasn't a valid password."
-            if not(check_verify):
-                error_verify = "Your passwords didn't match."
-            if not(check_email):
-                error_email = "That's not a valid email."
+        if not(check_username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+        if not(check_password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        if not(check_verify):
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+        if not(check_email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
 
-            # def write_form(self, username="", password="", verify="", email="", error_username="", error_password="", error_email="", error_verify=""):
-            self.write_form(username = escape_html(user_username), email=escape_html(user_email), error_username=error_username, error_password=error_password, 
-                                                                        error_verify=error_verify, error_email=error_email)
-            self.response.out.write(footer)
-            # self.response.out.write("invalid!")
+        if have_error:
+            self.render("signup_form.html", **params)
         else:
-            self.redirect("/unit2/welcome?username=" + escape_html(user_username))
-
+            self.redirect("/unit2/welcome?username=" + user_username)
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        username = self.request.get("username")
-        self.response.out.write(header)
-        self.response.out.write("<h2>Welcome, " + username + "!</h2>")
-        self.response.out.write(footer)
-
-class AboutHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write(header)
-        self.response.out.write("<p>Made as a part of CS253 - Web App Engg course on Udacity. Find the code on <a href='https://github.com/prakhar1989/CS253'>Github</a><p>")
-        self.response.out.write(footer)
-
+        self.response.out.write("<h2>Welcome, " + self.request.get("username") + "!</h2>")
 
 app = webapp2.WSGIApplication([('/', MainPage), 
-                               ('/thanks', ThanksHandler),
-                               ('/about', AboutHandler),
                                ('/unit2/rot13', RotHandler),
                                ('/unit2/signup', SignupHandler),
                                ('/unit2/welcome', WelcomeHandler)], debug=True)
-
-
